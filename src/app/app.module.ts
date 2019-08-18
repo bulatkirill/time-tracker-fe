786ad3374babe4ae079ae0e79031c295a837becf
bulatkyr <bulatkyr@fit.cvut.cz>
2019-08-18 19:50:37 +0200

    Init + before implementing Http CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {TimeEntriesComponent} from './time-entries/time-entries.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TimeEntriesComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +20,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
